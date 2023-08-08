a71ecc6a27aa9d3dd12ac0665a30ca9687c60c2f
firefly293 <advaitbadrish@gmail.com>
2023-08-07 17:58:55 -0700

    minor change

--- clearsaves.py
+++ clearsaves.py
@@ -6,6 +6,6 @@ for file_path in glob.iglob(file_pattern):
     try:
         with open(file_path, 'w') as file:
             file.truncate(0)
-        print(f"Emptied: {file_path}")
+        print(f"emptied {file_path}")
     except Exception as e:
-        print(f"Error emptying {file_path}: {e}")
+        print(f"error emptying {file_path}: {e}")
